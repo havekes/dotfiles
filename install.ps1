6d44ec25d2154051b0e17cce282a04a14e61cfc5
Greg Havekes <883845+havekes@users.noreply.github.com>
2021-07-06 22:14:09 -0400

    feat: basic scripts from mac laptop

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$CONFIG = "install.conf.yaml"
+$CONFIG = "config/win.yaml"
 $DOTBOT_DIR = "dotbot"
 
 $DOTBOT_BIN = "bin/dotbot"
--- install.sh
+++ install.sh
@@ -2,14 +2,23 @@
 
 set -e
 
-CONFIG="install.conf.yaml"
-DOTBOT_DIR="dotbot"
+# Choose config depending on OS
+if [ $(uname -s) == Darwin ]; then
+    CONFIG="config/macos.yaml"
+elif [ $(uname -r) == "*WSL2" ]; then
+    CONFIG="config/wsl.yaml"
+else
+    CONFIG="config/linux.yam"
+fi
 
+DOTBOT_DIR="dotbot"
 DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Download submodules
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
+# Run dotbot
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
