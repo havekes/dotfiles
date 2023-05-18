48aff18ed8c2484ba3751ae59a63638f4f3b36c6
= <=>
2023-05-18 08:20:51 -0400

    update local setup

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,2 +1,3 @@
 defaults write com.apple.dock expose-animation-duration -float 0.1
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
